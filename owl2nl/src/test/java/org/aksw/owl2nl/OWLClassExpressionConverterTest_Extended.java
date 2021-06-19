ef8c563fcfbea568c07cda37c3a188a051c38253
AparnaSivaprasad <aparnasivaprasad@gmail.com>
2021-06-19 18:26:45 +0200

    Fixed test cases that were failing due to null pointer exception.

--- owl2nl/src/test/java/org/aksw/owl2nl/OWLClassExpressionConverterTest_Extended.java
+++ owl2nl/src/test/java/org/aksw/owl2nl/OWLClassExpressionConverterTest_Extended.java
@@ -44,8 +44,8 @@ public class OWLClassExpressionConverterTest_Extended {
 	private static OWLDataRange dataRange;
 
 	OWLClassExpression ce;
-	OWLClassExpression complexce1;
-	OWLClassExpression complexce2;
+	OWLClassExpression ce1;
+	OWLClassExpression ce2;
 
 	String text;
 
@@ -105,27 +105,32 @@ public class OWLClassExpressionConverterTest_Extended {
 	public void testNested1() {
 		/*someone who works for at least 5 companies that is ledby a company or a person*/
 		ce = df.getOWLObjectMinCardinality(5, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person))));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person))));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("≥ 5 worksFor.(Company ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 5 that a company that is led by a company or a person", text);
+		Assert.assertEquals("something that works for at least 5 that a company that is" +
+				" led by a company or a person", text);
 		//String expected = "someone who works for at least 5 companies that is ledby a company or a person";
 		//Assert.assertEquals(expected, text);
 
 		/*someone who works for at least one company which is led by a company or a person*/
 		ce = df.getOWLObjectMinCardinality(1, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person))));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person))));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("≥ 1 worksFor.(Company ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 1 that a company that is led by a company or a person", text);
+		Assert.assertEquals("something that works for at least 1 that a company that is" +
+				" led by a company or a person", text);
 	}
 
 	@Test
 	public void simpleNegation() {
 		/*someone who does not work for a person or a company*/
-		ce = df.getOWLObjectComplementOf(df.getOWLObjectSomeValuesFrom(worksFor, df.getOWLObjectUnionOf(person, company)));
+		ce = df.getOWLObjectComplementOf(df.getOWLObjectSomeValuesFrom(worksFor,
+				df.getOWLObjectUnionOf(person, company)));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("¬(∃ worksFor.(Company ⊔ Person))", ce.toString());
@@ -136,29 +141,38 @@ public class OWLClassExpressionConverterTest_Extended {
 	public void testNested2() {
 
 		ce = df.getOWLObjectMinCardinality(5, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person))));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person))));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("≥ 5 worksFor.(Company ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 5 that a company that is led by a company or a person", text);
+		Assert.assertEquals("something that works for at least 5 that a company that is" +
+				" led by a company or a person", text);
 
 		ce = df.getOWLObjectMinCardinality(1, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person))));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person))));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("≥ 1 worksFor.(Company ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 1 that a company that is led by a company or a person", text);
+		Assert.assertEquals("something that works for at least 1 that a company that is" +
+				" led by a company or a person", text);
 	}
 
 	@Test
 	public void complexNegationWithMaxCardinality() {
 		/*someone who does not work for a person or a company and whose birthplace is paderborn that has not more than 50000 inhabitants */
-		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(birthPlace, paderborn), df.getOWLDataMaxCardinality(500000, nrOfInhabitants, dataRange));
-		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectComplementOf(df.getOWLObjectSomeValuesFrom(worksFor, df.getOWLObjectUnionOf(person, company)))), ce);
+		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(birthPlace, paderborn),
+				df.getOWLDataMaxCardinality(500000, nrOfInhabitants, dataRange));
+		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectComplementOf(
+				df.getOWLObjectSomeValuesFrom(worksFor, df.getOWLObjectUnionOf(person, company)))), ce);
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("((¬(∃ worksFor.(Company ⊔ Person)))) ⊓ ((∃ birthPlace.{Paderborn}) ⊓ (≤ 500000 nrOfInhabitants.()))", ce.toString());
-		Assert.assertEquals("something that something that does not works not for a company or a person and that something whose birth place is Paderborn and that has at most 500000 nr of inhabitants that are greater than or equals to 10000000", text);
+		Assert.assertEquals("((¬(∃ worksFor.(Company ⊔ Person)))) ⊓ ((∃ birthPlace.{Paderborn})" +
+				" ⊓ (≤ 500000 nrOfInhabitants.()))", ce.toString());
+		Assert.assertEquals("something that something that does not works not for a company" +
+				" or a person and that something whose birth place is Paderborn and that has" +
+				" at most 500000 nr of inhabitants that are greater than or equals to 10000000", text);
 	}
 
 	@Test
@@ -166,30 +180,37 @@ public class OWLClassExpressionConverterTest_Extended {
 
 		/*someone who works for at least 5 companies which are not software companies and are ledby a company or a person*/
 		ce = df.getOWLObjectMinCardinality(5, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person)), df.getOWLObjectComplementOf(softwareCompany)));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person)), df.getOWLObjectComplementOf(softwareCompany)));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("≥ 5 worksFor.(Company ⊓ (¬SoftwareCompany) ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 5 that a company that are not a software company and that are led by a company or a person", text);
+		Assert.assertEquals("≥ 5 worksFor.(Company ⊓ (¬SoftwareCompany)" +
+				" ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
+		Assert.assertEquals("something that works for at least 5 that a company that are" +
+				" not a software company and that are led by a company or a person", text);
 		/*someone who works for at least one company which is not a software company and led by a company or a person*/
 		ce = df.getOWLObjectMinCardinality(1, worksFor,
-				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy, df.getOWLObjectUnionOf(company, person)), df.getOWLObjectComplementOf(softwareCompany)));
+				df.getOWLObjectIntersectionOf(company, df.getOWLObjectSomeValuesFrom(ledBy,
+						df.getOWLObjectUnionOf(company, person)), df.getOWLObjectComplementOf(softwareCompany)));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("≥ 1 worksFor.(Company ⊓ (¬SoftwareCompany) ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
-		Assert.assertEquals("something that works for at least 1 that a company that is not a software company and that is led by a company or a person", text);
-		//System.out.println("Expected: someone who works for at least one company which is not a software company and led by a company or a person");
+		Assert.assertEquals("≥ 1 worksFor.(Company ⊓ (¬SoftwareCompany)" +
+				" ⊓ (∃ isLedBy.(Company ⊔ Person)))", ce.toString());
+		Assert.assertEquals("something that works for at least 1 that a company that is" +
+				" not a software company and that is led by a company or a person", text);
 	}
 
 	@Test
 	public void testDataHasValueAndObjectHasValue() {
 		// works for a company
 		/*someone whose work place is paderborn and whose amount of salary is 40000*/
-		ce = df.getOWLObjectIntersectionOf(df.getOWLDataHasValue(amountOfSalary, salary), df.getOWLObjectHasValue(workPlace, paderborn));
+		ce = df.getOWLObjectIntersectionOf(df.getOWLDataHasValue(amountOfSalary, salary),
+				df.getOWLObjectHasValue(workPlace, paderborn));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("(∃ workPlace.{Paderborn}) ⊓ (∃ amountOfSalary.{40000})", ce.toString());
-		Assert.assertEquals("something that works place Paderborn and whose amount of salary is 40000", text);
+		Assert.assertEquals("something that works place Paderborn" +
+				" and whose amount of salary is 40000", text);
 	}
 
 	@Test
@@ -219,17 +240,19 @@ public class OWLClassExpressionConverterTest_Extended {
 	@Test
 	public void testComplex8() {
 		// a person that sings karaoke or a person that sings  jazz
-		complexce1 = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
-		text = converter.convert(complexce1);
-		System.out.println(complexce1 + " = " + text);
-//		Assert.assertEquals("Person ⊓ (∃ sing.{karaoke})",ce.toString());
-//		Assert.assertEquals("a person that sings karaoke",text);
+		ce1 = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke),
+				person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
+		text = converter.convert(ce1);
+		System.out.println(ce1 + " = " + text);
+		Assert.assertEquals("(Person ⊓ (∃ sing.{jazz})) ⊔ (Person ⊓ (∃ sing.{karaoke}))", ce1.toString());
+		Assert.assertEquals("a person that sings jazz or karaoke", text);
 	}
 
 	@Test
 	public void testComplex7() {
 		// a person that sings karaoke or jazz
-		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke), df.getOWLObjectHasValue(sings, Jazz)), person);
+		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke),
+				df.getOWLObjectHasValue(sings, Jazz)), person);
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
 		Assert.assertEquals("Person ⊓ ((∃ sing.{jazz}) ⊓ (∃ sing.{karaoke}))", ce.toString());
@@ -239,58 +262,81 @@ public class OWLClassExpressionConverterTest_Extended {
 	@Test
 	public void testComplex6() {
 		// a person that sings karaoke and a person that sings jazz
-		complexce2 = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
-		text = converter.convert(complexce2);
-		System.out.println(complexce2 + " = " + text);
-		Assert.assertEquals("Person ⊓ ((∃ sing.{jazz}) ⊓ (∃ sing.{karaoke}))", ce.toString());
+		ce2 = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings,
+				karaoke), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
+		text = converter.convert(ce2);
+		System.out.println(ce2 + " = " + text);
+		Assert.assertEquals("(Person ⊓ (∃ sing.{jazz})) ⊓ (Person ⊓ (∃ sing.{karaoke}))",
+				ce2.toString());
 		Assert.assertEquals("something that a person that sings jazz and karaoke", text);
 	}
 
 	@Test
 	public void testComplex5() {
 		// a person that plays Cricket or a person that plays football or a person that plays hockey.
-		ce = ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, football), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, hockey), person));
+		ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket),
+				person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, football), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, hockey), person));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ (Person ⊓ (∃ play.{football})) ⊔ (Person ⊓ (∃ play.{hockey}))", ce.toString());
+		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ (Person ⊓ (∃ play.{football}))" +
+				" ⊔ (Person ⊓ (∃ play.{hockey}))", ce.toString());
 		Assert.assertEquals("a person that plays cricket, football or hockey", text);
 	}
 
 	@Test
 	public void testmultipleConnectors() {
 		// sentences for multiple connectors
-		ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, football), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, hockey), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, tennis), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, golf), person));
+		ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket),
+				person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, football), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, hockey), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, tennis), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, golf), person));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ (Person ⊓ (∃ play.{football})) ⊔ (Person ⊓ (∃ play.{golf})) ⊔ (Person ⊓ (∃ play.{hockey})) ⊔ (Person ⊓ (∃ play.{tennis}))", ce.toString());
+		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ (Person ⊓ (∃ play.{football}))" +
+				" ⊔ (Person ⊓ (∃ play.{golf})) ⊔ (Person ⊓ (∃ play.{hockey})) ⊔ (Person ⊓ (∃ play.{tennis}))",
+				ce.toString());
 		Assert.assertEquals("a person that plays cricket, football, golf, hockey or tennis", text);
 	}
 
 	@Test
 	public void testComplex2() {
-		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, rock), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, hiphop), person));
+		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke),
+				person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, rock), person),
+				df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, hiphop), person));
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("(Person ⊓ (∃ sing.{hiphop})) ⊓ (Person ⊓ (∃ sing.{jazz})) ⊓ (Person ⊓ (∃ sing.{karaoke})) ⊓ (Person ⊓ (∃ sing.{rock}))", ce.toString());
+		Assert.assertEquals("(Person ⊓ (∃ sing.{hiphop})) ⊓ (Person ⊓ (∃ sing.{jazz}))" +
+				" ⊓ (Person ⊓ (∃ sing.{karaoke})) ⊓ (Person ⊓ (∃ sing.{rock}))", ce.toString());
 		Assert.assertEquals("something that a person that sings hiphop, jazz, karaoke and rock", text);
 	}
 
 	@Test
 	public void testComplex3() {
-		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, rock), person), complexce1);
+		ce1 = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, karaoke),
+				person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
+		ce = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, rock),
+				person), ce1);
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("(Person ⊓ (∃ sing.{rock})) ⊓ ((Person ⊓ (∃ sing.{jazz})) ⊔ (Person ⊓ (∃ sing.{karaoke})))", ce.toString());
-		Assert.assertEquals("something that a person that sings rock and that a person that sings jazz or a person that sings karaoke", text);
+		Assert.assertEquals("(Person ⊓ (∃ sing.{rock})) ⊓ ((Person ⊓ (∃ sing.{jazz}))" +
+				" ⊔ (Person ⊓ (∃ sing.{karaoke})))", ce.toString());
+		Assert.assertEquals("something that a person that sings rock and jazz or karaoke", text);
 	}
 
 	@Test
 	public void testComplex4() {
-		ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket), person), complexce2);
+		ce2 = df.getOWLObjectIntersectionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings,
+				karaoke), person), df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(sings, Jazz), person));
+		ce = df.getOWLObjectUnionOf(df.getOWLObjectIntersectionOf(df.getOWLObjectHasValue(plays, Cricket),
+				person), ce2);
 		text = converter.convert(ce);
 		System.out.println(ce + " = " + text);
-		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ ((Person ⊓ (∃ sing.{jazz})) ⊓ (Person ⊓ (∃ sing.{karaoke})))", ce.toString());
-		Assert.assertEquals("a person that plays cricket or something that a person that sings jazz and that a person that sings karaoke", text);
+		Assert.assertEquals("(Person ⊓ (∃ play.{cricket})) ⊔ ((Person ⊓ (∃ sing.{jazz}))" +
+				" ⊓ (Person ⊓ (∃ sing.{karaoke})))", ce.toString());
+		Assert.assertEquals("a person that plays cricket or sings jazz and sings karaoke", text);
 
 	}
 }
